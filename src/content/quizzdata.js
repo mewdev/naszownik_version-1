c792f484237e7334e86d19179e0f583c663114cb
mewdev <107949187+mewdev@users.noreply.github.com>
2024-04-24 19:35:35 +0200

    Initial commit

---
+++ src/content/quizzdata.js
@@ -0,0 +1,82 @@
+export const questions = [
+  {
+    question: 'cichy',
+    answers: [
+      { text: 'hluchý', correct: false },
+      { text: 'tichý', correct: true },
+      { text: 'silný', correct: false },
+    ],
+  },
+  {
+    question: 'długi',
+    answers: [
+      { text: 'dlouhý', correct: true },
+      { text: 'kratký', correct: false },
+      { text: 'malý', correct: false },
+    ],
+  },
+  {
+    question: 'francuski',
+    answers: [
+      { text: 'anglický', correct: false },
+      { text: 'francouzský', correct: true },
+      { text: 'německý', correct: false },
+    ],
+  },
+  {
+    question: 'głowy',
+    answers: [
+      { text: 'hlavy', correct: true },
+      { text: 'ruk', correct: false },
+      { text: 'nohy', correct: false },
+    ],
+  },
+  {
+    question: 'hiszpański',
+    answers: [
+      { text: 'italský', correct: false },
+      { text: 'španělský', correct: true },
+      { text: 'portugalský', correct: false },
+    ],
+  },
+  {
+    question: 'jabłko',
+    answers: [
+      { text: 'hruška', correct: false },
+      { text: 'jabloň', correct: false },
+      { text: 'jablko', correct: true },
+    ],
+  },
+  {
+    question: 'kąpiel',
+    answers: [
+      { text: 'koupel', correct: true },
+      { text: 'van', correct: false },
+      { text: 'sprcha', correct: false },
+    ],
+  },
+  {
+    question: 'łatwy',
+    answers: [
+      { text: 'snadný', correct: true },
+      { text: 'těžký', correct: false },
+      { text: 'neradný', correct: false },
+    ],
+  },
+  {
+    question: 'maszyna',
+    answers: [
+      { text: 'stroj', correct: true },
+      { text: 'auto', correct: false },
+      { text: 'nábytek', correct: false },
+    ],
+  },
+  {
+    question: 'nowy',
+    answers: [
+      { text: 'nový', correct: true },
+      { text: 'starý', correct: false },
+      { text: 'opotřebený', correct: false },
+    ],
+  },
+];
